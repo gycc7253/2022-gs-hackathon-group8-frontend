8da7fe043e47bf26e17508118540f1e1a062e8db
gycc7253 <457776447@qq.com>
2022-07-12 20:17:27 +0800

    reverted back to unremoved file

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,127 @@
-import { React, useState } from "react";
+import { React, useState, setState, useRef, Component, createRef} from "react";
 import SearchBox from "../components/SearchBox";
-import Result from "../components/Result"
 import Image from 'next/image'
+import {Paper, TableContainer, Table, TableRow, TableHead, TableCell, TableBody} from '@mui/material'
+import { resultData } from "../../search-ui/src/component/pages/SearchResult/SearchResultData";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton"
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
 
-function HomePage() {
-    return (
-        // To solve image not showing
-        <div className="main">
-            <div className="header">
-            <Image src="/images/gs.jpg" width={150} height={150}/>
-            <span className="projectTitle">Integrated Engineering Search</span>
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {list: [], textFieldValue: ''};
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this)
+    }
+
+    handleTextFieldChange(event) {
+        this.setState({
+            textFieldValue: event.target.value
+        });
+    }
+
+
+    handleSearch() {
+        const url = 'https://qv5krif365.execute-api.ap-southeast-1.amazonaws.com/dev/search-query-tagger-lambda'
+
+        console.log("fetching")
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({'question': this.state.textFieldValue})
+        }).then(response => {
+            console.log(response.status)
+            if(response.ok){
+              const data = response.json()
+              return data
+            }else{
+              alert("something is wrong")
+            }
+          }).then(newResults => {
+            console.log(newResults)
+            console.log(typeof newResults)
+            console.log(newResults.length)
+            var d = [];
+            var keys = Object.keys(newResults)
+            keys.forEach(function(key){
+                d.push(newResults[key])
+            })
+            this.setState({list: d})
+            console.log(typeof this.state.list)
+          })
+
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <div className="header">
+                <Image src="/images/gs.jpg" width={150} height={150}/>
+                <span className="projectTitle">Integrated Engineering Search</span>
+                </div>
+                <div className="search">
+                    <form>
+                        <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        label="Search"
+                        onChange={this.handleTextFieldChange}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton color="primary" onClick={this.handleSearch}>
+                                        <SearchIcon/>
+                                    </IconButton>
+                                </InputAdornment>
+                                ),
+                        }}
+                        />
+                        <IconButton 
+                            type="submit" 
+                            onClick={this.handleSearch}/>
+                    </form>
+                </div>
+                <div>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 850 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center"> Id</TableCell>
+                                <TableCell align="center"> Title</TableCell>
+                                <TableCell align="center"> Page Url</TableCell>
+                                <TableCell align="center"> Source</TableCell>
+                                <TableCell align="center"> author</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.list.map(item => 
+                                <TableRow>
+                                    <TableCell>
+                                        {item.id}
+                                    </TableCell>
+                                    <TableCell>
+                                        {item.title}
+                                    </TableCell>
+                                    <TableCell>
+                                        {item.pageUrl}
+                                    </TableCell>
+                                    <TableCell>
+                                        {item.source}
+                                    </TableCell>
+                                    <TableCell>
+                                        {item.author}
+                                    </TableCell>
+                                </TableRow>)}
+                        </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
             </div>
-            <SearchBox/>
-            <Result/>
-        </div>
-    )
+        )
+    }
+
 }
-  
+
 export default HomePage
